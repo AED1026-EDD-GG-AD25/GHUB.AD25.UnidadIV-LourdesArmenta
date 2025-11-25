de2d61f905fad6c4445550b8e3eab9bc68234861
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2025-11-25 16:55:58 +0000

    iniciando paquete grafo

--- grafo/Vertice.java
+++ grafo/Vertice.java
@@ -1,5 +1,25 @@
 package grafo;
 
 public class Vertice {
+    private String  nombre;
+
+    public Vertice(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre ;
+    }
+    
+
     
 }
